0fb43bec30db71c0250e507911462bae783dfa2e
Sam Sabah <sam.sabah@exxeta.com>
2020-11-23 00:14:09 +0100

    Adding Implicitly Unwrapped Optionals

--- SwiftNinja/DetaleView.swift
+++ SwiftNinja/DetaleView.swift
@@ -5,7 +5,7 @@
 //  Created by Sabah, Sam on 21.11.20.
 //
 
-//["","","","","","", "","","",","","","","","Implicitly Unwrapped Optionals", "Error Handling", "Assertions and Preconditions","Debugging with Assertions","Enforcing Preconditions" ]
+//["","","","","","", "","","",","","","","","", "Error Handling", "Assertions and Preconditions","Debugging with Assertions","Enforcing Preconditions" ]
 
 
 import SwiftUI
@@ -757,6 +757,69 @@ struct DetaleView: View {
                         .multilineTextAlignment(.leading)
                     
                     Image("OptionalBinding003")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipped()
+    
+                }
+            }
+            
+            else if contact.name == "Implicitly Unwrapped Optionals" {
+                VStack (alignment: .leading){
+                    Text(contact.name)
+                        .bold()
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.leading)
+                    
+                    
+                    Text("""
+                    As described above, optionals indicate that a constant or variable is allowed to have “no value”. Optionals can be checked with an if statement to see if a value exists, and can be conditionally unwrapped with optional binding to access the optional’s value if it does exist.
+
+                    Sometimes it’s clear from a program’s structure that an optional will always have a value, after that value is first set. In these cases, it’s useful to remove the need to check and unwrap the optional’s value every time it’s accessed, because it can be safely assumed to have a value all of the time.
+
+                    These kinds of optionals are defined as implicitly unwrapped optionals. You write an implicitly unwrapped optional by placing an exclamation point (String!) rather than a question mark (String?) after the type that you want to make optional. Rather than placing an exclamation point after the optional’s name when you use it, you place an exclamation point after the optional’s type when you declare it.
+
+                    Implicitly unwrapped optionals are useful when an optional’s value is confirmed to exist immediately after the optional is first defined and can definitely be assumed to exist at every point thereafter. The primary use of implicitly unwrapped optionals in Swift is during class initialization, as described in Unowned References and Implicitly Unwrapped Optional Properties.
+
+                    An implicitly unwrapped optional is a normal optional behind the scenes, but can also be used like a non-optional value, without the need to unwrap the optional value each time it’s accessed. The following example shows the difference in behavior between an optional string and an implicitly unwrapped optional string when accessing their wrapped value as an explicit String:
+                    """)
+                        .multilineTextAlignment(.leading)
+                    
+                    Image("Unwrapped001")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipped()
+                    
+                    Text("""
+                    You can think of an implicitly unwrapped optional as giving permission for the optional to be force-unwrapped if needed. When you use an implicitly unwrapped optional value, Swift first tries to use it as an ordinary optional value; if it can’t be used as an optional, Swift force-unwraps the value. In the code above, the optional value assumedString is force-unwrapped before assigning its value to implicitString because implicitString has an explicit, non-optional type of String. In code below, optionalString doesn’t have an explicit type so it’s an ordinary optional.
+
+                    """)
+                        .multilineTextAlignment(.leading)
+                    
+                    Image("Unwrapped002")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipped()
+                    
+                    Text("""
+                    If an implicitly unwrapped optional is nil and you try to access its wrapped value, you’ll trigger a runtime error. The result is exactly the same as if you place an exclamation point after a normal optional that doesn’t contain a value.
+
+                    You can check whether an implicitly unwrapped optional is nil the same way you check a normal optional:
+                    """)
+                        .multilineTextAlignment(.leading)
+                    
+                    Image("Unwrapped003")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipped()
+                    
+                    Text("""
+                    You can also use an implicitly unwrapped optional with optional binding, to check and unwrap its value in a single statement:
+
+                    """)
+                        .multilineTextAlignment(.leading)
+                    
+                    Image("Unwrapped004")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .clipped()
