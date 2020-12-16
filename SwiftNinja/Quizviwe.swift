1480383fdffb41326bec37a7dc911c4b27ec9b1f
Sam Sabah <sam.sabah@exxeta.com>
2020-12-16 17:06:37 +0100

    bevor Git

--- SwiftNinja/Quizviwe.swift
+++ SwiftNinja/Quizviwe.swift
@@ -17,15 +17,15 @@ struct Quizviwe: View {
                             QuizOneRaw(qustions: contact)
                 }
             }
-            
-            
+            .navigationBarTitle("Which of the following is valid swift",displayMode: .inline)
 
-            
+
+
 
         }
-        .navigationBarTitle("Wich of the floowing is valuid swift",displayMode: .inline)
-            .font(.system(size: 12))
 
+
+        
     }
 }
 
@@ -41,94 +41,46 @@ struct Quizviwe_Previews: PreviewProvider {
 struct QuizOneRaw: View {
     
     let qustions:QuizOne
-    @State private var didTap:Bool = false
-    @State var score = 0
-
+    @State private var didTap: Bool = false
+    @State private var score : Bool = true
     var body: some View {
         HStack {
             Button(action: {
                 self.didTap.toggle()
-                }) {
-                
-                
-                
+                culcScore()
+      
+            }) {
                 Text(qustions.qustion)
                     .frame(minWidth: 40, maxWidth: .infinity, minHeight: 40, maxHeight: .infinity, alignment: .topLeading)
                     .font(.system(size: 21, weight:.medium, design: .default))
                     
                     .background(didTap && qustions.result ? Color.blue : didTap && !qustions.result ? Color.red : Color.clear)
-
             }
-
+            
+            if didTap && qustions.result {
+                Image("check")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40, alignment: .center)
+                    
+            }
+            else if didTap && !qustions.result {
+                Image("redx")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40, alignment: .center)
+                
+            }
+            
+            
         }
-
+                
+    }
+    
+    func culcScore(){
+        if self.didTap {
+            print(score)
+        }
         
-        if didTap && qustions.result {
-            Image("check")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 40, height: 40, alignment: .center)
-
-
-        }
-        else if didTap && !qustions.result {
-            Image("redx")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 40, height: 40, alignment: .center)
-        }
-
     }
 }
-
-
-/*
- 
- import SwiftUI
-
- struct ContatnView: View {
-     var body: some View {
-             List(contacts) { contact in
-                 NavigationLink(destination: DetaleView(contact: contact)){
-                     ContactRaw(contant: contact)
-                 }
-             }
-     
-     }
- }
-
- struct ContatnView_Previews: PreviewProvider {
-     static var previews: some View {
-         ContatnView()
-     }
- }
-
- struct ContactRaw: View {
-     
-     let contant:Contact
-     var body: some View {
-         HStack {
-   
-             
-             VStack(alignment: .leading){
-                 Text(contant.name)
-                     .font(.system(size: 21, weight:.medium, design: .default))
-                 Text("Scoure:\(contant.scoure)")
-                     Spacer()
-             }
-             Spacer()
-             if contant.completed{
-                 Image("check")
-                     .resizable()
-                     .aspectRatio(contentMode: .fill)
-                     .frame(width: 30, height: 30)
-                     .clipped()
-                     .cornerRadius(50)
-                     
-             }
-         }
-     }
- }
-
- */
-//
